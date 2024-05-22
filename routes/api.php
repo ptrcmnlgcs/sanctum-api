b5a324cc8157fb4a67b6af2eaa63c0012d109130
ptrcmnlgcs <gecosopatricia32@.gmail.com>
2024-05-22 16:44:53 +0800

    posts routes

--- routes/api.php
+++ routes/api.php
@@ -3,11 +3,11 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\PostController;
 
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
-
-
-
-
+Route::group(['middleware' => ['auth:sanctum']], function () {
+    Route::get('/posts', [PostController::class, 'posts']);
+});
 
